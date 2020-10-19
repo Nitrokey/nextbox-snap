a555eb57f3eda103b4e2d42f1c2335ab829ebb70
Markus Meissner <coder@safemailbox.de>
2020-10-19 18:56:28 +0200

    startscript + nameing

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-/snap/nextbox/current/usr/bin/python3 /snap/nextbox/current/bin/start.py
+/snap/nextbox/current/usr/bin/python3 /snap/nextbox/current/lib/python3.6/site-packages/nextbox-daemon/start.py
