33007dc888ec3f0beebd34d2d8a6e77062dc433e
Markus Meissner <coder@safemailbox.de>
2020-10-19 18:54:24 +0200

    add setup.py and generic packaging

---
+++ nextbox-daemon/setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "NextBox Daemon",
+    version = "0.0.1",
+    author = "Markus Meissner - Nitrokey",
+    author_email = "meissner@nitrokey.com",
+    description = "The Nitrokey - NextBox System Control Daemon",
+    license = "GPL",
+    packages = ["nextbox-daemon"],
+    classifiers=[
+        "Development Status :: 3 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GPL License",
+    ],
+)
